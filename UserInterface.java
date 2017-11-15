05d8fae1748495e6a49a255d6244d37e06d8b217
Unknown <3077-916@online.de>
2017-11-15 14:47:27 +0100

    Changes the UserInterface to seperate everything into functions and not everything in the main

--- UserInterface.java
+++ UserInterface.java
@@ -12,6 +12,67 @@ import java.util.Scanner;
  */
 public class UserInterface {
 
+    private int number;
+
+    /**
+     * This function gets the user input and saves it in the String input
+     *
+     * @return This returns the saved user input
+     */
+    public String getInput() {
+        System.out.println("Please type in a number: ");
+        Scanner x = new Scanner(System.in);
+        String input = x.next();
+        return input;
+    }
+
+    /**
+     * This function checks if the input is either q or Q to terminate.
+     *
+     * @param input This is the input from the user
+     * @return Either returns true if the input is q or Q or false if it isn't
+     */
+    public boolean checkForTermination(String input) {
+        if (input.contentEquals("Q") || input.contentEquals("q")) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This function converts the String input into a an Integer if possible
+     *
+     * @param input This is the input string
+     * @return Returns either true if the conversion was successfull or false if
+     * it wasn't
+     */
+    public boolean convertToInt(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            this.number = number;
+            return true;
+        } catch (NumberFormatException numberFormatException) {
+            System.out.println("Wrong Input");
+            return false;
+        }
+    }
+
+    /**
+     * This function prints the number it gets in binary formar using the
+     * StoreDecimal functions
+     *
+     * @param number This is the number the user entered
+     */
+    public void printBinary(int number) {
+        if (-128 <= number && number <= 127) {
+            StoreDecimal decimal = new StoreDecimal(number);
+            System.out.println(decimal.getBinary() + " "
+                    + decimal.getDecimal());
+        } else {
+            System.out.println("Wrong Number");
+        }
+    }
+
     /**
      * This is the main function. It runs until the user enters either a "q" or
      * "Q".It asks the user for a decimal number input in the 8 Bit Range. Its
@@ -22,28 +83,17 @@ public class UserInterface {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int number;
+
+        UserInterface userInterface = new UserInterface();
         while (true) {
-            System.out.println("Please type in a number: ");
-            Scanner x = new Scanner(System.in);
-            String input = x.next();
-            if (input.contentEquals("Q") || input.contentEquals("q")) {
+            String input = userInterface.getInput();
+            if (userInterface.checkForTermination(input)) {
                 break;
             }
-            try {
-                number = Integer.parseInt(input);
-            } catch (NumberFormatException numberFormatException) {
-                System.out.println("Wrong Input");
+            if (!(userInterface.convertToInt(input))) {
                 continue;
-
             }
-            if (-128 <= number && number <= 127) {
-                StoreDecimal decimal = new StoreDecimal(number);
-                System.out.println(decimal.getBinary() + " "
-                        + decimal.getDecimal());
-            } else {
-                System.out.println("Wrong Number");
-            }
+            userInterface.printBinary(userInterface.number);
         }
     }
 }
