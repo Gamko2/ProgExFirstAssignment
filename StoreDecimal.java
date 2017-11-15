ddf66b25556f661af1ef41c5b5f40a816e5d2aee
Unknown <3077-916@online.de>
2017-11-15 13:45:09 +0100

    Fixed spacing -again- and fixed JavaDoc

--- StoreDecimal.java
+++ StoreDecimal.java
@@ -1,52 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package firstturninjava;
 
 /**
- * This program implements the StoreDecimal class which does the conversion
- * from decimal to binary.
+ * This program implements the StoreDecimal class which does the conversion from
+ * decimal to binary.
+ *
  * @version 1.0
  * @author Marc Bambey
  * @since 14.11.2017
  */
+public class StoreDecimal {
 
-
-public class StoreDecimal  {
     private int number;
-    
-    /**
- * This is the constructor 
- * @param number is the decimal number you get when calling this function
- * This constructor just saves the number you entered in the private variable
- * number of the StoreDecimal class
- * @author Marc
- */
-    public StoreDecimal(int number){
-        this.number = number;
 
+    /**
+     * This is the constructor This constructor just saves the number you
+     * entered in the private variable number of the StoreDecimal class
+     *
+     * @param number is the decimal number you get when calling this function
+     * @author Marc
+     */
+    public StoreDecimal(int number) {
+        this.number = number;
     }
+
     /**
      * This function calculates the Binary value of the number entered.
+     *
      * @return this returns the Binary String calculated
      */
-    
-    public String getBinary(){
-        String  tmp = "";
+    public String getBinary() {
+        String tmp = "";
         for (int x = 7; x >= 0; x--) {
             tmp += ((1 << x) & number) != 0 ? "1" : "0";
         }
-        return  tmp;
+        return tmp;
     }
+
     /**
      * This returns the decimal number entered by the user
+     *
      * @return This just returns the decimal number
      */
-    
-    public int getDecimal(){
+    public int getDecimal() {
         return number;
     }
 }
--- UserInterface.java
+++ UserInterface.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package firstturninjava;
 
 import java.util.Scanner;
@@ -11,42 +5,43 @@ import java.util.Scanner;
 /**
  * This program implements the UserInterface for converting a decimal number
  * into a binary number.
+ *
  * @version 1.0
  * @author Marc Bambey
  * @since 14.11.2017
  */
-
 public class UserInterface {
 
     /**
-     * This is the main function. It runs until the user enters either
-     * a "q" or "Q".It asks the user for a decimal number input in the
-     * 8 Bit Range. Its throwing an exception if the number is not a 
-     * number or out of range of 8 Bit. If it is a legit number it 
-     * uses the functions from the StoreDecimal class to calculate the binary.
+     * This is the main function. It runs until the user enters either a "q" or
+     * "Q".It asks the user for a decimal number input in the 8 Bit Range. Its
+     * throwing an exception if the number is not a number or out of range of 8
+     * Bit. If it is a legit number it uses the functions from the StoreDecimal
+     * class to calculate the binary.
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         int number;
-        while (true){
-            System.out.println("Please type in a number: "); 
+        while (true) {
+            System.out.println("Please type in a number: ");
             Scanner x = new Scanner(System.in);
             String input = x.next();
-            if (input.contentEquals("Q")||input.contentEquals("q")){
+            if (input.contentEquals("Q") || input.contentEquals("q")) {
                 break;
             }
-            try{
-                number=Integer.parseInt(input);
-            }catch (NumberFormatException numberFormatException) {
-				System.out.println("Wrong Input");
-				continue;
-        
+            try {
+                number = Integer.parseInt(input);
+            } catch (NumberFormatException numberFormatException) {
+                System.out.println("Wrong Input");
+                continue;
+
             }
-            if (-128 <= number && number <= 127)    {
+            if (-128 <= number && number <= 127) {
                 StoreDecimal decimal = new StoreDecimal(number);
-                System.out.println(decimal.getBinary()+" " 
-                        + decimal.getDecimal());          
-            }else {
+                System.out.println(decimal.getBinary() + " "
+                        + decimal.getDecimal());
+            } else {
                 System.out.println("Wrong Number");
             }
         }
